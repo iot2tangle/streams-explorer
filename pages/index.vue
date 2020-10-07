1499233f17c3d6a359e420d162349774228df1fc
huhn511 <sehe89@gmail.com>
2020-10-07 21:41:55 +0200

    add streams explorer

---
+++ pages/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline"> stream-decoder </v-card-title>
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field
+              v-model="model"
+              :counter="max"
+              :rules="rules"
+              label="channel address"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+
+          <v-btn :disabled="loading" @click="read_channel" color="primary">
+            Read Channel
+          </v-btn>
+        </v-card-actions>
+
+        <v-progress-linear
+          v-if="loading"
+          color="primary"
+          indeterminate
+          rounded
+          height="8"
+        ></v-progress-linear>
+
+        <v-card
+          v-for="(entry, index) in data_entries"
+          :key="index"
+          class="mx-auto"
+          outlined
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="overline mb-4">
+                {{
+                  $moment(entry.timestamp * 1000).format(
+                    "MMMM Do YYYY, hh:mm:ss"
+                  )
+                }}
+              </div>
+              <v-list-item-subtitle
+                >Device ID: {{ entry.device }}</v-list-item-subtitle
+              >
+              <div v-for="(item, index) in entry.xdk2mam" :key="index">
+                <p>{{ item.sensor }}</p>
+                <pre>{{ item.data }}</pre>
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+  data: () => ({
+    data_entries: [],
+    loading: false,
+    allowSpaces: false,
+    max: 80,
+    model:
+      "1c6fddc2f0344892403ebc4fb6b94c4b308147c9fddb6340a27c89b7c4a28c390000000000000000",
+  }),
+  computed: {
+    rules() {
+      const rules = [];
+
+      if (this.max) {
+        const rule = (v) =>
+          (v || "").length <= this.max ||
+          `A maximum of ${this.max} characters is allowed`;
+
+        rules.push(rule);
+      }
+
+      if (!this.allowSpaces) {
+        const rule = (v) =>
+          (v || "").indexOf(" ") < 0 || "No spaces are allowed";
+
+        rules.push(rule);
+      }
+
+      return rules;
+    },
+  },
+
+  watch: {
+    max: "validateField",
+    model: "validateField",
+  },
+
+  methods: {
+    validateField() {
+      this.$refs.form.validate();
+    },
+    async read_channel({ $axios }) {
+      this.loading = true;
+      console.log("this channel: ", this.model);
+      let res;
+      try {
+        // res = await this.$axios.$get(
+        //   "http://localhost:8585/decode_channel", {data: this.model}
+        // );
+
+        res = await this.$axios
+          .$get("http://localhost:8585/decode_channel/" + this.model, {
+            headers: {
+              "Content-Type": "text/plain",
+            },
+          })
+          .then((response) => {
+            console.log(response);
+
+            if (response.status == "Success") {
+              this.data_entries = response.messages.map((x) => JSON.parse(x));
+            }
+            this.loading = false;
+          });
+      } catch (error) {
+        console.log("error: ", error);
+        this.loading = false;
+      }
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.logo {
+  width: 300px;
+}
+</style>
---
+++ pages/inspire.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class="headline">IOTA Streams </v-card-title>
+          <v-card-text>
+            <a href="https://github.com/iotaledger/streams">IOTA Streams</a> is
+            a work-in-progress framework for building cryptographic messaging
+            protocols. Streams ships with a built-in protocol called Channels
+            for sending authenticated messages between two or more parties on
+            the Tangle.
+          </v-card-text>
+        </v-card>
+        <v-card>
+          <v-card-title class="headline">StreamsDecoder </v-card-title>
+          <v-card-text>
+            <a href="https://github.com/AleBuser/streams_decoder"
+              >StreamsDecoder</a
+            >
+            decodes a streams channel
+          </v-card-text>
+        </v-card>
+        <v-card>
+          <v-card-title class="headline">iot2tangle </v-card-title>
+          <v-card-text>
+            <a href="http://iot2tangle.io/">iot2tangle</a>, an Open source hub
+            to integrate IoT devices with the Tangle
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
